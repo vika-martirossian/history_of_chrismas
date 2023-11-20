c827bd15b0824f46b7bd9b4c7d36e94b057eab81
vika-martirossian <vikamartirosian1@gmail.com>
2023-11-20 22:21:16 +0400

    test commit

--- christmas_history/histories_project/POM/tests/test_saturnalia_and_christmas.py
+++ christmas_history/histories_project/POM/tests/test_saturnalia_and_christmas.py
@@ -6,3 +6,6 @@ class TestSaturnaliaAndChristmas(BaseTest):
     def test_saturnalia_and_christmas_navigation(self):
         self.navigate_to_saturnalia_and_christmas_tab()
         self.check_the_second_tab_paragraph_and_go_back()
+
+    # test for git
+
